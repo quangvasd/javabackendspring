4cfee4d85322b8eec5c0b59db8854f55fb57e80d
quangvasd <doanquanghuyle@gmail.com>
2021-04-17 22:06:38 +0700

    create maven

---
+++ activity3/src/test/java/vn/plusplus/spring2/activity3/Activity3ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.plusplus.spring2.activity3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
